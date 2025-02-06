0da3b00a5eac25e77706539b586876dce36cb710
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-02-06 19:58:02 +0545

    addimg order model.js

--- tomato_food_delivery_app-main/backend/models/foodModel.js
+++ tomato_food_delivery_app-main/backend/models/foodModel.js
@@ -1,23 +1,3 @@
-// import mongoose from "mongoose";
-
-// const foodSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     description: { type: String, required: true },
-//     price: { type: Number, required: true},
-//     image: { type: String, required: true },
-//     category:{ type:String, required:true}
-// })
-
-// const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
-// export default foodModel;
-
-
-
-
-
-
-
-
 import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema({
@@ -27,4 +7,16 @@ const foodSchema = new mongoose.Schema({
     image: { type: String, required: true },
     category:{ type:String, required:true}
 })
+
+const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
+export default foodModel;
+
+
+
+
+
+
+
+
+
 
