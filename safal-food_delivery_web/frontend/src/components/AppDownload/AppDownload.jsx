dd7895f35cd828da25ba23d4e563ac0f86b9bc82
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-02-14 20:13:05 +0545

    adding app downloaded .jsx code

--- safal-food_delivery_web/frontend/src/components/AppDownload/AppDownload.jsx
+++ safal-food_delivery_web/frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,3 +1,51 @@
+// import React from 'react'
+// import './AppDownload.css'
+// import { assets } from '../../assets/assets'
+
+// const AppDownload = () => {
+//     return (
+//         <div className='app-download' id='app-download'>
+//             <p>For Better Experience Download <br />Tomato App</p>
+//             <div className="app-download-platforms">
+//                 <img src={assets.play_store} alt="" />
+//                 <img src={assets.app_store} alt="" />
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default AppDownload
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react'
 import './AppDownload.css'
 import { assets } from '../../assets/assets'
@@ -15,3 +63,7 @@ const AppDownload = () => {
 }
 
 export default AppDownload
+
+
+
+
