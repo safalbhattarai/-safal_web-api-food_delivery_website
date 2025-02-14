24f5b7af75c0c2b92d9b581be54427f155db2185
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-02-14 20:03:58 +0545

    adding backend code usermodel.js

--- safal-food_delivery_web/backend/models/userModel.js
+++ safal-food_delivery_web/backend/models/userModel.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
 }, { minimize: false })
 
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
-export default userModel;
+export default userModell;
 
 
 
