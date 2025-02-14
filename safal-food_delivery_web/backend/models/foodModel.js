5748e6048135b98ff2a755167b32b55e0257dd57
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-02-14 20:02:06 +0545

    adding food model.js code

--- safal-food_delivery_web/backend/models/foodModel.js
+++ safal-food_delivery_web/backend/models/foodModel.js
@@ -9,7 +9,7 @@ const foodSchema = new mongoose.Schema({
 })
 
 const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
-export default foodModel;
+export default foodModell;
 
 
 
