63b498107c43231c306b2e32e9b8f8573683cb25
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-02-14 20:17:18 +0545

    adding or updathin food routes.js code

--- safal-food_delivery_web/backend/routes/foodRoute.js
+++ safal-food_delivery_web/backend/routes/foodRoute.js
@@ -1,3 +1,32 @@
+// import express from 'express';
+// import { addFood, listFood, removeFood } from '../controllers/foodController.js';
+// import multer from 'multer';
+// const foodRouter = express.Router();
+
+// //Image Storage Engine (Saving Image to uploads folder & rename it)
+
+// const storage = multer.diskStorage({
+//     destination: 'uploads',
+//     filename: (req, file, cb) => {
+//         return cb(null,`${Date.now()}${file.originalname}`);
+//     }
+// })
+
+// const upload = multer({ storage: storage})
+
+// foodRouter.get("/list",listFood);
+// foodRouter.post("/add",upload.single('image'),addFood);
+// foodRouter.post("/remove",removeFood);
+
+// export default foodRouter;
+
+
+
+
+
+
+
+
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
@@ -19,6 +48,22 @@ foodRouter.post("/add",upload.single('image'),addFood);
 foodRouter.post("/remove",removeFood);
 
 export default foodRouter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
