8db1c51e60ddfcd2fbb556d760688b641afc7a3c
safalbhattarai <126142996+safalbhattarai@users.noreply.github.com>
2025-03-02 13:58:59 +0545

    changing in adding .jsx file

--- safal-food_delivery_web/admin/src/pages/Orders/Orders.jsx
+++ safal-food_delivery_web/admin/src/pages/Orders/Orders.jsx
@@ -179,3 +179,88 @@ const Order = () => {
 }
 
 export default Order
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useEffect, useState } from 'react'
+import './Orders.css'
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { assets, url } from '../../assets/assets';
+
+const Order = () => {
+
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(`${url}/api/order/list`)
+    if (response.data.success) {
+      setOrders(response.data.data.reverse());
+      console.log(response.data.data);
+    }
+    else {
+      toast.error("Error")
+    }
+  }
+
+  const statusHandler = async (event,orderId) => {
+    console.log(event,orderId);
+    const response = await axios.post(`${url}/api/order/status`,{
+      orderId,
+      status:event.target.value
+    })
+    if(response.data.success)
+    {
+      await fetchAllOrders();
+    }
+  }
+
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [])
+
+  return (
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className='order-item'>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className='order-item-food'>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity
+                  }
+                  else {
+                    return item.name + " x " + item.quantity + ", "
+                  }
+                })}
